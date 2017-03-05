e2a7119405de3c81db0ced760de5206a4ef103d3
LeaderOne <markelba1@gmail.com>
2017-03-05 13:44:42 -0600

    Form split

--- tfdpreplan/forms.py
+++ tfdpreplan/forms.py
@@ -8,9 +8,15 @@ from .utils import normalize_street_type
 
 
 class AddressForm(forms.Form):
-    address = forms.CharField(max_length=255)
+    street_no = forms.IntegerField(min_value=0)
+    street_dir = forms.RegexField(regex=r'^[NORnorSOUsouTHthEAeaWEweSTst]+$', min_length=1, error_message="Not a valid direction, check your spelling.")
+    street_name = forms.RegexField(regex=r'^[\w+ \'&\.]+', min_length=1, error_message="Not a valid street name.  Check for special characters.")
+    street_type = forms.RegexField(regex=r'^[\w+]+', min_length=1, error_message="Not a valid street type.  Check for special characters.")
+
 
     def clean_address(self):
+        self.cleaned_data['address'] = "%d %s %s %s".format(self.cleaned_data['street_no'], self.cleaned_data['street_dir'], self.cleaned_data['street_name'], self.cleaned_data['street_type'])
+
         parser = StreetAddressParser()
         parts = parser.parse(self.cleaned_data['address'])
         street_dir, street_name = parts['street_name'].split(' ', 1)
@@ -19,4 +25,3 @@ class AddressForm(forms.Form):
         self.cleaned_data['street_name'] = street_name
         self.cleaned_data['street_type'] = normalize_street_type(parts['street_type'])
         return self.cleaned_data['address']
-
