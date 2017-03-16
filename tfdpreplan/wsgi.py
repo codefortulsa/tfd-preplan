a7458dbe5000759f58919c89cfa0c789358492ee
Patrick Forringer <patrick@forringer.com>
2017-03-16 00:17:21 -0500

    Adding django-configurations

--- tfdpreplan/wsgi.py
+++ tfdpreplan/wsgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tfdpreplan.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tfdpreplan.settings')
+os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
+
+from configurations.wsgi import get_wsgi_application
 
 application = Cling(get_wsgi_application())
